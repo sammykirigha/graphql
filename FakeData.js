fc125b99446a58fe839b122414bcee948bb066b8
Samido <dkirigha18@gmail.com>
2021-11-17 12:25:44 +0300

    initialized a graphql app, added typeDefs and a fake data for users

---
+++ FakeData.js
@@ -0,0 +1,37 @@
+export const UserList = [
+  {
+    id: 1,
+    name: "john",
+    username: "John",
+    age: 20,
+    nationality: "Kenya",
+  },
+  {
+    id: 2,
+    name: "sammy",
+    username: "sammy",
+    age: 22,
+    nationality: "Tanzania",
+  },
+  {
+    id: 3,
+    name: "Dorcis",
+    username: "Dorcis",
+    age: 24,
+    nationality: "Uganda",
+  },
+  {
+    id: 4,
+    name: "Edwin",
+    username: "Edwin",
+    age: 25,
+    nationality: "Rwanda",
+  },
+  {
+    id: 5,
+    name: "Alex",
+    username: "Alex",
+    age: 26,
+    nationality: "Ethipia",
+  },
+];
--- scr/index.js
+++
@@ -1,2 +0,0 @@
-let firstName = "sammy";
-console.log(firstName);
